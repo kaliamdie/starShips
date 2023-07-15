2f2c0b920724bb194803dd2f368aa90094e9f0ad
kali <jamiegirl150@gmail.com>
2023-07-15 14:16:44 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+import NavaBar from './component/NavaBar'
+import StarShips from './component/StarShip'
+
+function App() {
+ 
+
+  return (
+    <div>
+      <NavaBar/>
+     <StarShips/>
+    </div>
+  )
+}
+
+export default App
